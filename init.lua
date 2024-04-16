758f43454e040a60e8464c2c2a973365b0a64a5b
Burak Demirtas <burak@burakdemirtas.io>
2024-04-16 08:05:04 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,2 @@
+require("helper.lazy")
+require("helper.keymaps")
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+		end,
+	},
+}
